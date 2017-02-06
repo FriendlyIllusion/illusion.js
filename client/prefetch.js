6b770d1dd1140730aaa88a40c513b9201e65902b
Jarzon <j@masterj.net>
2017-02-06 10:36:17 -0500

    Using this, to ref the link, instead of using a mutable variable

--- client/prefetch.js
+++ client/prefetch.js
@@ -6,15 +6,14 @@ Prefetch.prototype.hooks = function () {
     var links = document.querySelectorAll('a');
 
     for (var n = 0; n < links.length; n++) {
-        var link = links[n];
 
-        link.addEventListener('mousedown', function (event) {
+        links[n].addEventListener('mousedown', function (event) {
             // Left mouse click
             if (event.which == 1) {
                 var prefetch = document.createElement('link');
 
                 prefetch.rel = 'prefetch';
-                prefetch.href = link.href;
+                prefetch.href = this.href;
 
                 document.head.appendChild(prefetch);
             }
